6b4f4f9848379ed9a82a0763477d11b8b3bccc0d
Alex <alexeytitovwork@gmail.com>
2022-03-29 23:39:38 +0300

    infer

--- deserialize_model_to_get_engine.cpp
+++ deserialize_model_to_get_engine.cpp
@@ -28,20 +28,22 @@
 // #include "opencv2/cudawarping.hpp"
 #include <opencv2/opencv.hpp>
 #include "opencv2/cudaimgproc.hpp"
+#include “NvInfer.h”
 
+using namespace nvinfer1;
 // using namespace std;
 
 // using namespace std;
 
-// class Logger : public ILogger           
-// {
-//     void log(Severity severity, const char* msg) noexcept override
-//     {
-//         // suppress info-level messages
-//         if (severity <= Severity::kWARNING)
-//             std::cout << msg << std::endl;
-//     }
-// } logger;
+class Logger : public ILogger           
+{
+    void log(Severity severity, const char* msg) noexcept override
+    {
+        // suppress info-level messages
+        if (severity <= Severity::kWARNING)
+            std::cout << msg << std::endl;
+    }
+} logger;
 
 // class Logger : public nvinfer1::ILogger
 // {

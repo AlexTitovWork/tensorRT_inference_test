c8223b99a69aee255264a6f32a5fe4a350f7d126
Alex <alexeytitovwork@gmail.com>
2022-03-30 22:26:20 +0300

    add cuda include

--- deserialize_model_to_get_engine.cpp
+++ deserialize_model_to_get_engine.cpp
@@ -32,9 +32,8 @@
 // #include <opencv2/highgui/highgui.hpp>
 // #include <opencv2/imgproc/imgproc.hpp>
 
-// #include "opencv2/core.hpp"
-// #include "opencv2/cudawarping.hpp"
-#include <opencv2/cuda.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/cudawarping.hpp>
 #include <opencv2/cudaimgproc.hpp>
 
 
